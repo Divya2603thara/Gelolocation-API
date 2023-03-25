091cf9cae1c3cb03c15e8aa70c5ea52ca6a69c17
Divya2603thara <117896861+Divya2603thara@users.noreply.github.com>
2023-03-25 21:43:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var get = document.getElementById("get");
+var remove = document.getElementById("remove");
+var map = document.getElementById("map");
+
+let pos =  false;
+function getLocation() {
+  if (navigator.geolocation) {
+    if(localStorage.getItem('lat') || localStorage.getItem('long')){
+      pos = false;
+      showPosition(pos);
+    }
+    else{
+      pos=true;
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  }else {
+    map.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  if(!pos){
+    map.innerHTML="<iframe src='https://maps.google.com/maps?q="+localStorage.getItem('lat')+","+ localStorage.getItem('long')+"&z=15&output=embed' width='80%' height='600' frameborder='0' style='border:0'></iframe>";
+  }
+  else{
+  localStorage.setItem("lat",position.coords.latitude);
+  localStorage.setItem("long",position.coords.longitude);
+  map.innerHTML = "<iframe src='https://maps.google.com/maps?q="+position.coords.latitude+","+ position.coords.longitude+"&z=15&output=embed' width='80%' height='500' frameborder='0' style='border:0'></iframe>";
+}
+}
+
+function removelocation(){
+  if(localStorage.getItem('lat') || localStorage.getItem('long')){
+    localStorage.clear();
+  }else{
+    alert("No Location In Local Storage")
+  }
+}
+
+get.addEventListener('click',getLocation)
+remove.addEventListener('click',removelocation)
